3d76c4983a6a4e81ac58604e8a8cd04e9b4ef8e4
Anas <anaselnahef12@gmail.com>
2023-05-06 20:36:55 +0300

    add count carts

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -40,10 +40,15 @@
                 {{-- right hand navbar items  --}}
                 <div class="flex gap-x-4">
                     <livewire:search-input /> 
-                    <a href='{{ route('cart') }}'  class="hover:text-gray-400 text-gray-900 dark:hover:text-white" >
+                    <a href='{{ route('cart') }}'  class="relative hover:text-gray-400 text-gray-900 dark:hover:text-white" >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                         </svg>
+                        @auth
+                        @if (auth()->user()->carts->count() > 0)
+                        <div class="absolute inline-flex items-center justify-center text-center w-6 h-6 text-xs font-bold text-white bg-cyan-600 border-2 border-white rounded-full -top-3 -right-3 dark:border-gray-700">{{ auth()->user()->carts->count() }}</div>  
+                        @endif
+                        @endauth
                     </a>
                     
                     @guest
